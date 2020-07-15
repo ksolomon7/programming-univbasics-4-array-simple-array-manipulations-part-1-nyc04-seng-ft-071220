a1c6d0d93df3f7506e1cfd51ee9b290a698e8617
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-15 18:35:41 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  puts array.pop(2)
+end
+
+pop_with_args(["a","b","c","d","e"])
